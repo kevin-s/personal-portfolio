431cb7b45fdc755d41a84869923fc30017780a2b
Kevin <kevin-s@users.noreply.github.com>
2018-09-02 12:28:06 -0500

    adding some basic api calls and display logic

--- src/Review.js
+++ src/Review.js
@@ -4,12 +4,12 @@ import {Header} from './Header';
 
 
 export class Review extends Component {
-    render(match) {
+    render() {
         return (
             <div className="App">
                 <Header />
                 <div>
-                    <h3>{match.params.topicId}</h3>
+                    <h3>{this.match.params.topicId}</h3>
                 </div>
             </div>
         );
--- src/Reviews.js
+++ src/Reviews.js
@@ -3,33 +3,61 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import {Header} from './Header';
 import {Review} from './Review';
 
+const API = 'https://hn.algolia.com/api/v1/search?query=';
+const DEFAULT_QUERY = 'redux';
 
 export class Reviews extends Component {
-    render(match) {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hits: [],
+            isLoading: false,
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true });
+
+        fetch(API + DEFAULT_QUERY)
+            .then(response => response.json())
+            .then(data => this.setState({ hits: data.hits, isLoading: false }));
+    }
+
+    render() {
+
+        const { hits, isLoading } = this.state;
+
+        if (isLoading) {
+            return <p>Loading ...</p>;
+        }
+
         return (
             <div>
                 <Header />
                 <h2>Reviews</h2>
-                <ul>
-                    <li>
-                        <Link to={'${match.url}/rendering'}>
-                            Rendering with React
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to={'${match.url}/components'}>
-                            Components
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to={'${match.url}/props-v-state'}>
-                            Props v. State
-                        </Link>
-                    </li>
-                </ul>
 
-                <Route path={'${match.path}/:topicId'} component={Review}/>
-                <Route exact path='{match.path}' render={() => (
+                <ul>
+                    {hits.map(hit =>
+                        <li key={hit.objectID}>
+                            <a href={hit.url}>{hit.title}</a>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        );
+
+
+        return (
+            <div>
+                <Header />
+                <h2>Reviews</h2>
+
+
+
+                <Route path='{this.path}/:topicId' component={Review}/>
+                <Route exact path={this.path} render={() => (
                     <h3>Please select a topic.</h3>
                 )}/>
             </div>
