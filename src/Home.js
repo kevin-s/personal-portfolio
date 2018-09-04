8309c2c283c69cc987642cffc5dbe8c098a36d47
Kevin <kevin-s@users.noreply.github.com>
2018-09-03 21:11:49 -0500

    made some circles.  Not happy with them. :expressionless:

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import {Header} from './Header';
-
+import { Link } from 'react-router-dom';
+import "./Home.css";
 
 export class Home extends Component {
     render() {
         return (
-            <div className="App">
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>Link to Resume <Link to="/resume">Resume</Link></p>
-            </div>
+                    <div className="container">
+
+                        <article className="col">
+                            <div className="circle"><h1><a href="#">Problem-solver and big picture thinker.</a></h1></div>
+                        </article>
+                        <article className="col">
+                            <div className="circle"><h1><a href="#">Technical communicator and mentor.</a></h1></div>
+                        </article>
+                        <article className="col">
+                            <div className="circle"><h1><a href="#">Fast learner and faster at recovering from roadblocks.</a></h1></div>
+                        </article>
+
+                </div>
         );
     }
 }
